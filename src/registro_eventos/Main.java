e728b70ec8744a79d3722cb27f42db144104370d
Alan Castillo <aneculpan_@estudiantesunap.cl>
2025-06-09 23:09:17 -0400

    Proyecto completado

---
+++ src/registro_eventos/Main.java
@@ -0,0 +1,9 @@
+package registro_eventos;
+
+public class Main {
+    public static void main(String[] args) {
+            
+        FormularioRegistro formularioRegistro = new FormularioRegistro();
+        formularioRegistro.setVisible(true);
+    }
+}
